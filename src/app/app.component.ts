2399321c7bdaef159d4e44628d770be06ca25178
ABHINAtk <abhinashaji@gmail.com>
2023-11-22 21:39:51 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-directives';
+  DisplayTable:boolean = true
+
+  visible:boolean=false
+
+
+  onclick(){
+    this.DisplayTable=!this.DisplayTable;
+    this.visible=!this.visible;
+  }
+
+
+  columnStyles: { [key: number]: { [key: string]: string } } = {};
+
+  changeColumnStyle() {
+   
+    for (let i = 0; i < 4; i++) {
+      this.columnStyles[i] = { 'background-color': this.getRandomColor() };
+    }
+  }
+
+  getColumnStyle(columnIndex: number) {
+    return this.columnStyles[columnIndex];
+  }
+
+  getRandomColor() {
+    
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }
+
+
+  
+
+
+
+
+
+
